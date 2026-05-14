25252dcb55a2ea34441b051a39fe70fcf7df6c87
Sunil Shastry <sunil.shastry69@gmail.com>
2026-05-14 13:00:57 -0600

    Setup an initial Spring Boot project with the official Spring Initializr - currently unable to run due to non-existent database connection

---
+++ src/test/java/com/dinnerdibs/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinnerdibs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
