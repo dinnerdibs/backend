97cd4ed9f408990e54b33163f5e1e018aba462e6
Sunil Shastry <sunil.shastry69@gmail.com>
2026-05-15 15:48:01 -0600

    Create a standalone ApiVersions class to manage the API versioning with exposing the current stable version

---
+++ src/main/java/com/dinnerdibs/backend/config/api/ApiVersions.java
@@ -0,0 +1,16 @@
+package com.dinnerdibs.backend.config.api;
+
+/**
+ * A helper class to centralize the backend's API versioning structure
+ */
+public class ApiVersions {
+    /**
+     * A private static field for the first API version
+     */
+    private static final String API_VERSION_ONE = "v1";
+
+    /**
+     * Static field to set the current stable API version for the system
+     */
+    public static final String CURRENT_API_VERSION = API_VERSION_ONE;
+}
