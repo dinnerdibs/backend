ec5dbe2c92bcd43b971b9d250fef8a874e786bb8
Sunil Shastry <sunil.shastry69@gmail.com>
2026-05-15 12:42:58 -0600

    Include the missing documentation for the created enums

--- src/main/java/com/dinnerdibs/backend/user/enums/AccountStatus.java
+++ src/main/java/com/dinnerdibs/backend/user/enums/AccountStatus.java
@@ -1,5 +1,9 @@
 package com.dinnerdibs.backend.user.enums;
 
+/**
+ * Enumeration type for the User account status option
+ * A User's account status can either be ACTIVE, SUSPENDED, BANNED or PENDING at a given time
+ */
 public enum AccountStatus {
     ACTIVE,
     SUSPENDED,
--- src/main/java/com/dinnerdibs/backend/user/enums/Role.java
+++ src/main/java/com/dinnerdibs/backend/user/enums/Role.java
@@ -1,5 +1,9 @@
 package com.dinnerdibs.backend.user.enums;
 
+/**
+ * Enumeration type for the User account's role
+ * A User can have multiple roles at a given time
+ */
 public enum Role {
     CUSTOMER,
     CHEF,
