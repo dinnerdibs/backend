2d84ed0e6f14a9177eabd3db1e89fd31b0f86cfb
Sunil Shastry <sunil.shastry69@gmail.com>
2026-05-16 02:14:40 -0600

    Develop a helper DTO (Data Transfer Object) titled UserRegisterRequest for accepting fields for user registration operation

---
+++ src/main/java/com/dinnerdibs/backend/user/dto/request/UserRegisterRequest.java
@@ -0,0 +1,38 @@
+package com.dinnerdibs.backend.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * Data Transfer Object (DTO) for User register operation request
+ */
+@Data
+public class UserRegisterRequest {
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Past
+    private LocalDate dateOfBirth;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 4, max = 30)
+    private String username;
+
+    @NotBlank
+    @Size(min = 8, max = 100)
+    private String password;
+
+    private String displayName;
+}
