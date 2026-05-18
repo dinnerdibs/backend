63d034ed682074c7e09f12759596129dc91701df
Sunil Shastry <sunil.shastry69@gmail.com>
2026-05-17 21:06:57 -0600

    Create and setup the utility class SuccessMessages that helps with the success response messages for ApiResponse results

---
+++ src/main/java/com/dinnerdibs/backend/config/message/SuccessMessages.java
@@ -0,0 +1,26 @@
+package com.dinnerdibs.backend.config.message;
+
+/**
+ * A utility class managing the success messages/responses for the system
+ */
+public class SuccessMessages {
+    /**
+     * A custom success message when an entity is promptly created on the system
+     */
+    private final String CREATED = "Success: Entity was successfully created!";
+
+    /**
+     * A custom success message when an entity is correctly retrieved from the system
+     */
+    private final String RETRIEVED = "Success: Entity was successfully retrieved!";
+
+    /**
+     * A custom success message when an entity is correctly updated in the system
+     */
+    private final String UPDATED = "Success: Entity was successfully updated!";
+
+    /**
+     * A custom success message when an entity is correctly deleted from the system
+     */
+    private final String DELETED = "Success: Entity was successfully deleted!";
+}
