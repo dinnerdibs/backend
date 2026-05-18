0bbd8c961bbe54871512f2dd7efe0db473e6285e
Sunil Shastry <sunil.shastry69@gmail.com>
2026-05-17 21:07:58 -0600

    Update the access specifiers for the SuccessMessages from private to public and static

--- src/main/java/com/dinnerdibs/backend/config/message/SuccessMessages.java
+++ src/main/java/com/dinnerdibs/backend/config/message/SuccessMessages.java
@@ -7,20 +7,20 @@ public class SuccessMessages {
     /**
      * A custom success message when an entity is promptly created on the system
      */
-    private final String CREATED = "Success: Entity was successfully created!";
+    public static final String CREATED = "Success: Entity was successfully created!";
 
     /**
      * A custom success message when an entity is correctly retrieved from the system
      */
-    private final String RETRIEVED = "Success: Entity was successfully retrieved!";
+    public static final String RETRIEVED = "Success: Entity was successfully retrieved!";
 
     /**
      * A custom success message when an entity is correctly updated in the system
      */
-    private final String UPDATED = "Success: Entity was successfully updated!";
+    public static final String UPDATED = "Success: Entity was successfully updated!";
 
     /**
      * A custom success message when an entity is correctly deleted from the system
      */
-    private final String DELETED = "Success: Entity was successfully deleted!";
+    public static final String DELETED = "Success: Entity was successfully deleted!";
 }
